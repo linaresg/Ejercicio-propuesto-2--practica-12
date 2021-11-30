494cfcd496ef94f8d1728dfd19c58b9c35a16e29
linaresg <82194867+linaresg@users.noreply.github.com>
2021-11-30 13:25:07 -0600

    Add files via upload

---
+++ Code.php
@@ -0,0 +1,39 @@
+<?php
+if($_REQUEST['cod']==1)
+	echo "<p>Comunicacion Social</p>
+	<p>Educaciòn Inicial</p>
+	<p>Educacion Primaria</p>
+	<p>Educacion Secundaria</p>
+	<p>Psicologia</p>
+	<p>Publicidad y Multimedia</p>
+	<p>Teologia</p>
+	<p>Trabajo Social</p>
+	<p>Turismo y Hoteleria</p>";
+if($_REQUEST['cod']==2)
+	echo "<p>Administracion de Empresas</p>
+	<p>Ciencia Politica y Gobierno</p>
+	<p>Contabilidad</p>
+	<p>Derecho</p>
+	<p>Ingenieria Comercial</p>";
+if($_REQUEST['cod']==3)
+	echo "<p>Arquitectura</p>
+	<p>Ingenieria Agronomica y Agricola</p>
+	<p>Ingenieria Ambiental</p>
+	<p>Ingenieria Civil</p>
+	<p>Ingenieria de Industria Alimentaria</p>
+	<p>Ingenieria de Minas</p>
+	<p>Ingenieria de Sistemas</p>
+	<p>Ingenieria Electronica</p>
+	<p>Ingenieria Industrial</p>
+	<p>Ingenieria Mecanica</p>
+	<p>Ingenieria Mecanica Electrica</p>
+	<p>Ingenieria Mecatronica</p>
+	<p>Medicina Veterinaria y Zootecnia</p>";
+if($_REQUEST['cod']==4)
+	echo "<p>Enfermeria</p>
+	<p>Farmacia y Bioquimica</p>
+	<p><strong>Ingenieria Biotecnologica</p>
+	<p>Medicina Humana</p>
+	<p>Obstetricia y Puericultura</p>
+	<p>Odontologia</p>";
+?>
